43b81180f9d4d60c65a740df4dd71f0cecffd34e
Aed10 <eddyyanyayoub@gmail.com>
2023-04-28 00:22:41 +0000

    Update comment.c Handle the case of a string containing a # symbole.

--- comment.c
+++ comment.c
@@ -12,13 +12,16 @@ char *comment(char *input)
 
         for (i = 0; input[i]; i++)
         {
-                if (input[i] == '#')
+                if (input[0] == '#')
                 {
                         input[i] = '\0';
 
                 }
-                else
-                        input[i] = input[i];
+                else if (input[i] == '#' && input[i-1] == ' ')
+                        input[i] = '\0';
+		else
+			input[i] = input[i];
         }
+
         return (input);
 }
